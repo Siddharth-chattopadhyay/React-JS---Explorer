5f2623a17b62d579b50598787e2391b88d63ba3f
Siddharth Chattopadhyay <siddharth@192.168.1.187>
2026-01-29 12:50:42 +0530

    Explorer

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import {Checkbox} from "@heroui/checkbox";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import OffCanvas from "./components/OffCanvas.jsx"; // Ignore this error. It still works
+
+function App() {
+  return (
+    <>
+    <div className="p-5">
+      <h1>NextUI or HeroUI</h1>
+      <Checkbox defaultSelected>Checkbox</Checkbox>
+      <br />
+      <Checkbox isDisabled>Checkbox</Checkbox>
+      <br />
+      <Checkbox isDisabled defaultSelected>Checkbox</Checkbox>
+    </div>
+    <div className="p-5">
+      <h1>React Bootstrap</h1>
+      <OffCanvas/>
+    </div>
+    <div className="p-5">
+      <h1>TailWindCSS</h1>
+      <p className="text-3xl font-bold underline">Paragraph is styled using tailwindcss</p>
+    </div>
+    <div className="p-5">
+      <h1>Remix</h1>
+      <Remix/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Remix.jsx
@@ -0,0 +1,28 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import {HeroUIProvider} from "@heroui/react";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <HeroUIProvider>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </HeroUIProvider>
+      </body>
+    </html>
+  );
+}
